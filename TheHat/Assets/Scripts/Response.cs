7890ea6f23c250b10628e6d54f9e058ac4b0820a
Divyata Singh <divyatasngh@gmail.com>
2018-11-21 23:33:42 -0800

    relevance meters added

---
+++ TheHat/Assets/Scripts/Response.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Response : MonoBehaviour {
+
+    public class Skills
+    {
+        public int required;
+        public int matching;
+        public string[][] missing;
+    }
+
+    public class Experience
+    {
+        public int required;
+        public int current;
+    }
+
+    public class Metrics
+    {
+        public Skills skills;
+        public Experience experience;
+        public int score;
+    }
+
+    public class EducationDetails
+    {
+        public string degree;
+        public string university;
+        public string duration;
+    }
+
+    public class ExperienceDetails
+    {
+        public string position;
+        public string company;
+        public string duration;
+        public string description;
+    }
+    public string firstName;
+    public string id;
+    public string industry;
+    public string lastName;
+    public UnityEngine.Object positions;
+    public string[][] courses;
+    public string[][] skills;
+    public EducationDetails[] education;
+    public ExperienceDetails[] experience;
+    public Metrics metrics;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	
+}
---
+++ TheHat/Assets/Scripts/TotalScoreNeedleRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalScoreNeedleRotation : MonoBehaviour {
+    static float minAngle = 180;
+    static float maxAngle = 0;
+    static TotalScoreNeedleRotation thisMeter;
+
+    void Start () {
+        thisMeter = this;
+
+    }
+	
+	public static void ShowScore(float score)
+    {
+        float angle = Mathf.Lerp(minAngle, maxAngle, Mathf.InverseLerp(0, 100, score));
+        thisMeter.transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
