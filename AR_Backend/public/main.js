b81b366187ef756949e3dc7136aad346bfdfb85b
Dhanashri Tidke <36896018+dhanashritidke@users.noreply.github.com>
2018-11-22 17:15:50 -0800

    Add files via upload

---
+++ AR_Backend/public/main.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+      $('#save').click( function() {
+        var experience = $('#experience').val(),
+          education = $( "#degree" ).val(),
+          hw_engineer_skills = $("#hw_engineer").tagsinput('items'),
+          sw_engineer_skills = $("#sw_engineer").tagsinput('items'),
+          data_scientist_skills = $("#data_scientist").tagsinput('items'),
+          web_developer_skills = $("#web_developer").tagsinput('items'),
+          ugrad_required = false,
+          grad_required = false
+          
+        if(education == 'ugrad_required') {
+          ugrad_required = true
+        }
+        else {
+          ugrad_required = true
+          grad_required = true
+        }
+        var skillroleDictionary = {
+          "sw_engineer" : sw_engineer_skills,
+          "hw_engineer" : hw_engineer_skills,
+          "data_scientist" : data_scientist_skills,
+          "web_developer" : web_developer_skills,
+          "experience" : experience,
+          "grad_required": grad_required,
+          "ugrad_required" : ugrad_required
+        }
+        $.ajax({
+          url: 'http://localhost:8080/skills',
+          // dataType: "jsonp",
+          data: skillroleDictionary,
+          type: 'POST',
+          //jsonpCallback: 'callback', // this is not relevant to the POST anymore
+          success: function (data) {
+              var ret = jQuery.parseJSON(data);
+              $('#lblResponse').html(ret.msg);
+              console.log('Success: ')
+          },
+          error: function (xhr, status, error) {
+              console.log('Error: ' + error.message);
+              $('#lblResponse').html('Error connecting to the server.');
+          },
+        });
+      });
+});
